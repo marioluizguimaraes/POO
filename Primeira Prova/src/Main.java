ab498f4d445e097d936f477e7448c6680133df5f
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-11-14 09:45:10 -0300

    Prova 14/11/2024

---
+++ Primeira Prova/src/Main.java
@@ -0,0 +1,11 @@
+import Questoes.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+       // Q1.diasDaSemana(0);
+       // Q2.numeroPerfeito();
+        Q3.criptografia();
+
+    }
+}
