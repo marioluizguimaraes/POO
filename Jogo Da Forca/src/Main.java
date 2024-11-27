2cbded067f6e04a3c4ff7aaadee3b77cd7baff0a
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-11-27 08:59:01 -0300

    .

--- Jogo Da Forca/src/Main.java
+++ Jogo Da Forca/src/Main.java
@@ -6,14 +6,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int vidas = 0;
-        int acertos = 0;
+        //int vidas = 0;
+        //int acertos = 0;
 
         Boneco b1 = new Boneco();
         System.out.println(b1.apararecer());
+
+        b1.perderMembro(4,2);
+        System.out.println(b1.apararecer());
+
+        /*
         System.out.println("Palavra:");
 
-        Palavras p1 = new Palavras("C:\\Users\\mario\\Documents\\Codigos\\POO\\Jogo Da Forca\\src\\BancoDePalavras.txt");
+        Palavras p1 = new Palavras("C:\\Users\\20241014040032\\Documents\\POO\\Jogo Da Forca\\src\\BancoDePalavras.txt");
 
         String[] letras = p1.sortearPalavra().split("");
         int numDeLetras = letras.length;
@@ -26,5 +31,6 @@ public class Main {
         for (int i = 0; i < numDeLetras; i++){
             System.out.print(letras[i]);
         }
+        */
     }
 }
