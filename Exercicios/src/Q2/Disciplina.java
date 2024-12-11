06f07f5946da14c03a94c4ee5f902fb1f5c784be
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-12-11 09:55:46 -0300

    Update Disciplina.java

--- Exercicios/src/Q2/Disciplina.java
+++ Exercicios/src/Q2/Disciplina.java
@@ -80,7 +80,7 @@ public class Disciplina {
         }
         else if (this.notas[2] == -1){
             System.out.println("ATENÇÃO: o resultado não é válido, pois a nota final ainda não foi cadastrada!");
-            this.mediafinal = (this.notas[0] + this.notas[1] + this.notas[2])/3;
+            this.mediafinal = (this.notas[0] + this.notas[1] + 0)/3;
             return this.mediafinal;
         }
         else{
